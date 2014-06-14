dc93372fb0eef743fdee9eed4d6fe2f7c1a38b9d
hearsute <manuoval@gmail.com>
2014-06-14 16:09:47 +0200

    dfgdfg

--- app/header/login.php
+++ app/header/login.php
@@ -1,76 +1,29 @@
 <?php
-$erreurlogin = '';
-
-
-if(  (isset($_SESSION['adminConnected']) && $_SESSION['adminConnected'] == true) || (isset($_SESSION['memberConnected']) && $_SESSION['memberConnected'] == true)  )
+if (isset($_POST['login'], $_POST['password']))
 {
-    require('views/header/logged.html');
-}
-
-else if(isset($_POST['login'], $_POST['pass']))
-{
-                
-
-               
-                    $login = mysqli_real_escape_string($mysqli,$_POST['login']);
-                    $pass = mysqli_real_escape_string($mysqli,$_POST['pass']);
-
-                    
-
-
-                    
-                    $verif= "SELECT `login`, `id`, `admin`, `pass` FROM bugs.user WHERE  `login` = '".$login."' AND `pass` = '".$pass."' ";
-
-                    $verifsql= mysqli_query($mysqli, $verif);
-                    $ligneverif = mysqli_fetch_assoc($verifsql);
-
-                            if($ligneverif !== null)
-                            {
-                                $admin = $ligneverif['admin'];
-                                
-                                $login = htmlentities($ligneverif['login']);
-                                $_SESSION['login'] = $login;
-
-                                $_SESSION['loginid'] = $ligneverif['id'];
-                                 
-
-                                        /*| N'y aurait-il pas une meilleure facon de faire ? :) |*/
-                                        /*| On utilise deux boolean pour savoir si on est admin ou pas... |*/
-                                        if($admin==1)
-                                        {
-                                            $_SESSION['adminConnected'] = true;
-                                            $_SESSION['memberConnected'] = false;
-
-                                        }
-                                        else
-                                        {
-                                            $_SESSION['adminConnected'] = false;
-                                            $_SESSION['memberConnected'] = true;
-                                        }
-
-                                require('views/header/logged.html');
-
-                            }
-                            else
-                            {
-                                $erreurlogin = 'login ou mot de passe incorrect'; 
-                                require('views/header/login.html');
-                            }
-
+    $manager = new UserManager($db);
+    $user = $manager->getUserByLogin($_POST['login']);
+    if ($user)
+    {
+        if ($user->passwordVerify($_POST['password']))
+        {
+            $_SESSION['id'] = $user->getId();
+            $_SESSION['prenom'] = $user->getPrenom();
+            $_SESSION['nom'] = $user->getNom();
+            header('Location:index.php');
+        }
+        else
+        {
+            echo "mdp invalide";
+            // pass incorrect => gestion d'erreur : mdp invalide
+        }
+    }
+    else
+    {
+        echo "login invalide";
+        // Login incorrect => gestion erreur : login pas dans la db
+    }
 }
 else
-{
-
-$_SESSION['adminConnected'] = false;
-$_SESSION['memberConnected'] = false;
-$_SESSION['loginid'] = 404;
-$erreurlogin = 'connectez vous :';
-require('views/header/login.html');
-}
-
-
-
-
-
-	
+    require('app/content/signup.php');
 ?>
