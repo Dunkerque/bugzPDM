12f3763994c903b1dc280cb6dfc8a1c7ca996f16
Dunkerque <dannypacheco1990@gmail.com>
2014-06-14 15:31:48 +0200

    ""

--- config.php
+++ config.php
@@ -1,13 +1,8 @@
 <?php
-//define("FOO", "something");
 $db_user = 'root';
 $db_pass = '';
 $db_database = 'bugz';
 $db_connector = 'mysql';
 $db_port = '3306';
 $db_host = 'localhost';
-/*
-$lang = 'fr-FR';
-$version = '1.2.3';//soleil
-*/
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 header('Content-Type: text/html; charset=utf-8');
-require('conf.php');
+require('config.php');
 $classPath = scandir("model/");
 function __autoload($classname)
 {
