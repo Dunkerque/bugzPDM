2aa8d0ac148873f06b943a91a29db5ecaeec64ba
hearsute <manuoval@gmail.com>
2014-06-14 14:43:28 +0200

    ...

---
+++ datas/BLOGMVC/app/login.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST['login'], $_POST['password']))
+{
+	$manager = new UserManager($db);
+	$user = $manager->getUserByLogin($_POST['login']);
+	if ($user)
+	{
+		if ($user->passwordVerify($_POST['password']))
+		{
+			$_SESSION['id'] = $user->getId();
+			$_SESSION['prenom'] = $user->getPrenom();
+			$_SESSION['nom'] = $user->getNom();
+			header('Location:index.php');
+		}
+		else
+		{
+			echo "mdp invalide";
+			// pass incorrect => gestion d'erreur : mdp invalide
+		}
+	}
+	else
+	{
+		echo "login invalide";
+		// Login incorrect => gestion erreur : login pas dans la db
+	}
+}
+else
+	require('views/login.html');
+?>
