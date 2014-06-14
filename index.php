2479fa7239b85604cbb836c7dca2098015f20d32
Dunkerque <dannypacheco1990@gmail.com>
2014-06-14 15:58:23 +0200

    ""

--- index.php
+++ index.php
@@ -35,60 +35,12 @@ $secuViews =  array("home" => "content/home",
 $page = 'content/home';
 if (isset($_GET['page']))
 {
-//    $views = strtolower(htmlentities($_GET['page']));
-    if (  isset($secuViews[$_GET['page']]) )
-            $page = $secuViews[$_GET['page']];
+    $views = strtolower(htmlentities($_GET['page']));
+    if (isset($secuViews[$views]) )
+            $page = $secuViews[$views];
     else
         $page = '404';
 }
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']))
     require('app/'.$page.'.php');
 require('app/skel.php');
-
-
-
-//<?php
-//session_start();
-//header("Content-Type: text/html; charset=utf-8");
-//require('app/server.php');
-//$page = "home";
-//
-//$mysqli = mysqli_connect("localhost", "root","troiswa", "bugs");
-///*| Il faut vérifier si la connection mysql a reussi ! |*/
-//
-//
-//$tab = array("home" => "content/home",
-//    "carte"=>"content/carte",
-//    "compte"=>"content/compte",
-//    "coord" =>"content/coord",
-//    "paiement"=>"content/paiement",
-//    "signup"=>"content/signup",
-//    "reservation"=>"content/reservation",
-//    "voirpanier"=>"content/voirpanier",
-//    "livre"=>"content/livre",
-//    "logout"=>"logout",
-//    "comptemodif"=>"content/comptemodif");
-//
-//	if(isset($_GET['page']))
-//    {
-//
-//        if (  isset($tab[$_GET['page']]) )
-//            $page = $tab[$_GET['page']];
-//
-//        else
-//        {
-//            $page = "404";
-//        }
-//    }
-//
-//    else
-//    {
-//        header('location:index.php?page=home');
-//    }
-//
-// require("app/skel.php");
-//
-//
-//
-//
-?>
