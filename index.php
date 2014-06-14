639df27ce3ef94e078a6081e6eedf48045546147
Dunkerque <dannypacheco1990@gmail.com>
2014-06-14 15:29:14 +0200

    ""

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+require('conf.php');
+$classPath = scandir("model/");
+function __autoload($classname)
+{
+    $classname = strtolower($classname);
+    if(in_array($classname, $classPath) === true)
+    require('model/'.$classname.'.class.php');
+    return false;
+}
+try
+{
+    $db = new PDO($db_connector.':host='.$db_host.';port='.$db_port.';dbname='.$db_database, $db_user, $db_pass);
+}
+catch (Exception $e)
+{
+    die("Internal Error");
+}
+
+$secuViews = scandir("apps/");
+
+if (isset($_GET['page']))
+{
+    $views = strtolower(htmlentities($_GET['page']));
+    if (in_array($views, $secuViews) === true)
+        $page = $views;
+    else
+        $page = '404';
+}
+else
+    $page = 'home';
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']))
+    require('app/'.$page.'.php');
+require('app/skel.php');
+?>
