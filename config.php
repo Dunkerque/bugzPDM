f14aac7eaf002a73dfe6848f787593cdfd504df3
hearsute <manuoval@gmail.com>
2014-06-15 13:41:22 +0200

    Revert "configmac"
    This reverts commit 1c4e58e6cc4e7d138f3209742fa7a02bb8827f9b.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$db_user = 'root';
+$db_pass = '';
+$db_database = 'bugz';
+$db_connector = 'mysql';
+$db_port = '3306';
+$db_host = 'localhost';
+?>
---
+++ datas/BLOGMVC/conf.php
@@ -0,0 +1,13 @@
+<?php
+//define("FOO", "something");
+$db_user = 'root';
+$db_pass = '';
+$db_database = 'blog';
+$db_connector = 'mysql';
+$db_port = '3306';
+$db_host = 'localhost';
+/*
+$lang = 'fr-FR';
+$version = '1.2.3';//soleil
+*/
+?>
